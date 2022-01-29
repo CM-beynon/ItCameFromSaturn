ff32cd1d2ac4eda858921ddb8c5f28b1c4980578
lel-rc <60361950+lel-rc@users.noreply.github.com>
2022-01-29 15:55:07 -0600

    Added buttons

--- Scenes/Button.gd
+++ Scenes/Button.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var can_press
+var been_pressed = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -12,9 +14,20 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	if can_press:
+		if Input.is_action_pressed("ui_accept"):
+			been_pressed = true;
+	if been_pressed:
+		$Pressed.show()
+		$Prompt.hide()
 
 func _on_Button_body_entered(body):
-	emit_signal("prompt")
+	if !been_pressed:
+		$Prompt.show()
+	can_press = true
+
+
+func _on_Button_body_exited(body):
+	$Prompt.hide()
+	can_press = false
